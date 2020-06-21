00671a7cd0c7e832b2554bafc9f225720ac77faf
kuldeepsarnaik <sarnaik.kuldeep1@gmail.com>
2020-06-21 18:35:17 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/Controller/MyController.java
@@ -0,0 +1,15 @@
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/Login")
+public class MyController {
+
+
+ public String getName()
+ {
+     return  "something for now";
+
+ }
+
+}
