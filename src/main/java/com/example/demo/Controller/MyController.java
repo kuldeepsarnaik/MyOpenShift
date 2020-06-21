2145bf75206a2e730c90f8e1daedd936094d1dbc
kuldeepsarnaik <sarnaik.kuldeep1@gmail.com>
2020-06-21 19:00:16 +0530

    Added Dummy Method

--- src/main/java/com/example/demo/Controller/MyController.java
+++ src/main/java/com/example/demo/Controller/MyController.java
@@ -1,11 +1,21 @@
 package com.example.demo.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/Login")
+@RestController
+
 public class MyController {
 
+    @RequestMapping("/")
+    public String Start()
+    {
+        return  "something for now";
 
+    }
+
+
+    @RequestMapping("/Login")
  public String getName()
  {
      return  "something for now";
