180c4950b9348e06c982624fe5caac4924c638be
kuldeepsarnaik <sarnaik.kuldeep1@gmail.com>
2020-06-23 21:54:15 +0530

    Push  My app

--- src/main/java/com/example/demo/Controller/MyController.java
+++ src/main/java/com/example/demo/Controller/MyController.java
@@ -1,11 +1,22 @@
 package com.example.demo.Controller;
 
+import com.example.demo.dao.UserRepository;
+import com.example.demo.model.user;
+import com.example.demo.service.IuserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 
 public class MyController {
+
+    @Autowired
+    private IuserService userService;
 
     @RequestMapping("/")
     public String Start()
@@ -21,5 +32,17 @@ public class MyController {
      return  "something for now";
 
  }
+
+    @GetMapping("/showuser")
+    public List<user> findCities() {
+
+        List<user> users = (List<user>) userService.findAll();
+
+       // model.addAttribute("cities", user);
+
+        return users;
+    }
+
+
 
 }
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,27 @@
 package com.example.demo;
 
+import com.example.demo.dao.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
-public class DemoApplication {
+
+public class DemoApplication  {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+
 	}
 
-}
+
+	}
+
+
